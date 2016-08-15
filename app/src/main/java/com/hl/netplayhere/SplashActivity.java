3c9629ea9fe177bafcd288751eaec04378600c35
yongjiaming <yongjiaming@meizu.com>
2016-08-15 21:08:30 +0800

    添加bilibili弹幕框架

---
+++ app/src/main/java/com/hl/netplayhere/SplashActivity.java
@@ -0,0 +1,33 @@
+package com.hl.netplayhere;
+
+import android.content.Intent;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.ImageView;
+
+/**
+ * An example full-screen activity that shows and hides the system UI (i.e.
+ * status bar and navigation/system bar) with user interaction.
+ */
+public class SplashActivity extends AppCompatActivity {
+    ImageView imageView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        imageView = (ImageView) findViewById(R.id.bg_image);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(SplashActivity.this, LoginActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        }, 1000);
+
+
+    }
+}
